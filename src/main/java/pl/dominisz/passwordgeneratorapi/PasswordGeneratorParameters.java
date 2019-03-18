a4a2641521b26cfe97d3a9b0cf01eae9bd39daee
dominisz <dominisz@gmail.com>
2019-03-18 20:27:21 +0100

    Add constructors, getters and setters

--- src/main/java/pl/dominisz/passwordgeneratorapi/PasswordGeneratorParameters.java
+++ src/main/java/pl/dominisz/passwordgeneratorapi/PasswordGeneratorParameters.java
@@ -2,11 +2,62 @@ package pl.dominisz.passwordgeneratorapi;
 
 public class PasswordGeneratorParameters {
 
-    int passwordLength;
-    boolean includeSymbols;
-    boolean includeNumbers;
-    boolean includeLowercaseCharacters;
-    boolean includeUppercaseCharacters;
+    private int passwordLength;
+    private boolean includeSymbols;
+    private boolean includeNumbers;
+    private boolean includeLowercaseCharacters;
+    private boolean includeUppercaseCharacters;
 
+    public void setPasswordLength(int passwordLength) {
+        this.passwordLength = passwordLength;
+    }
+
+    public void setIncludeSymbols(boolean includeSymbols) {
+        this.includeSymbols = includeSymbols;
+    }
+
+    public void setIncludeNumbers(boolean includeNumbers) {
+        this.includeNumbers = includeNumbers;
+    }
+
+    public void setIncludeLowercaseCharacters(boolean includeLowercaseCharacters) {
+        this.includeLowercaseCharacters = includeLowercaseCharacters;
+    }
+
+    public void setIncludeUppercaseCharacters(boolean includeUppercaseCharacters) {
+        this.includeUppercaseCharacters = includeUppercaseCharacters;
+    }
+
+    public PasswordGeneratorParameters() {
+    }
+
+    public PasswordGeneratorParameters(int passwordLength, boolean includeSymbols, boolean includeNumbers, boolean includeLowercaseCharacters,
+                                       boolean includeUppercaseCharacters) {
+        this.passwordLength = passwordLength;
+        this.includeSymbols = includeSymbols;
+        this.includeNumbers = includeNumbers;
+        this.includeLowercaseCharacters = includeLowercaseCharacters;
+        this.includeUppercaseCharacters = includeUppercaseCharacters;
+    }
+
+    public int getPasswordLength() {
+        return passwordLength;
+    }
+
+    public boolean isIncludeSymbols() {
+        return includeSymbols;
+    }
+
+    public boolean isIncludeNumbers() {
+        return includeNumbers;
+    }
+
+    public boolean isIncludeLowercaseCharacters() {
+        return includeLowercaseCharacters;
+    }
+
+    public boolean isIncludeUppercaseCharacters() {
+        return includeUppercaseCharacters;
+    }
 }
 
