4c04627ec280d128aa2df9bb79cec5e0b785e056
dominisz <dominisz@gmail.com>
2019-03-18 19:50:56 +0100

    Initial commit

---
+++ src/main/java/pl/dominisz/passwordgeneratorapi/PasswordGeneratorParameters.java
@@ -0,0 +1,12 @@
+package pl.dominisz.passwordgeneratorapi;
+
+public class PasswordGeneratorParameters {
+
+    int passwordLength;
+    boolean includeSymbols;
+    boolean includeNumbers;
+    boolean includeLowercaseCharacters;
+    boolean includeUppercaseCharacters;
+
+}
+
