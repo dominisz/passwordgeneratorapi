d5d54fc47d2e9244122ac69ace9a7f09a75bac00
dominisz <dominisz@gmail.com>
2019-04-08 20:35:07 +0200

    Use @Data and @Builder from Lombok

--- src/main/java/pl/dominisz/passwordgeneratorapi/PasswordGeneratorParameters.java
+++ src/main/java/pl/dominisz/passwordgeneratorapi/PasswordGeneratorParameters.java
@@ -1,5 +1,10 @@
 package pl.dominisz.passwordgeneratorapi;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PasswordGeneratorParameters {
 
     private int passwordLength;
@@ -8,56 +13,5 @@ public class PasswordGeneratorParameters {
     private boolean includeLowercaseCharacters;
     private boolean includeUppercaseCharacters;
 
-    public void setPasswordLength(int passwordLength) {
-        this.passwordLength = passwordLength;
-    }
-
-    public void setIncludeSymbols(boolean includeSymbols) {
-        this.includeSymbols = includeSymbols;
-    }
-
-    public void setIncludeNumbers(boolean includeNumbers) {
-        this.includeNumbers = includeNumbers;
-    }
-
-    public void setIncludeLowercaseCharacters(boolean includeLowercaseCharacters) {
-        this.includeLowercaseCharacters = includeLowercaseCharacters;
-    }
-
-    public void setIncludeUppercaseCharacters(boolean includeUppercaseCharacters) {
-        this.includeUppercaseCharacters = includeUppercaseCharacters;
-    }
-
-    public PasswordGeneratorParameters() {
-    }
-
-    public PasswordGeneratorParameters(int passwordLength, boolean includeSymbols, boolean includeNumbers, boolean includeLowercaseCharacters,
-                                       boolean includeUppercaseCharacters) {
-        this.passwordLength = passwordLength;
-        this.includeSymbols = includeSymbols;
-        this.includeNumbers = includeNumbers;
-        this.includeLowercaseCharacters = includeLowercaseCharacters;
-        this.includeUppercaseCharacters = includeUppercaseCharacters;
-    }
-
-    public int getPasswordLength() {
-        return passwordLength;
-    }
-
-    public boolean isIncludeSymbols() {
-        return includeSymbols;
-    }
-
-    public boolean isIncludeNumbers() {
-        return includeNumbers;
-    }
-
-    public boolean isIncludeLowercaseCharacters() {
-        return includeLowercaseCharacters;
-    }
-
-    public boolean isIncludeUppercaseCharacters() {
-        return includeUppercaseCharacters;
-    }
 }
 
